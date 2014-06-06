e583dd0ce001c35aa23dd41abe2a415702fc0e7f
James Ruan <ruanbeihong@gmail.com>
2014-06-06 22:48:48 +0800

    add collection-music

---
+++ music-collection.ml.js
@@ -0,0 +1,19 @@
+\h3{music}
+
+Collection for music related resources.
+
+\hline
+
+\h4{theory}
+
+\h5{无调性理论介绍}
+from \link+[nonhounnome@果壳]{http://www.guokr.com/i/1517102603/activities/}
+\list*{
+    \* \link+[无调性理论简介（一）前言]{http://www.guokr.com/post/575063/}
+    \* \link+[无调性理论简介（二）音和音程]{http://www.guokr.com/post/576640/}
+    \* \link+[无调性理论简介（三）音集和音程]{http://www.guokr.com/post/577233/}
+    \* \link+[无调性理论简介（四）音集的变形与基本集]{http://www.guokr.com/post/577912/}
+    \* \link+[无调性理论简介（五）音集理论的实际应用]{http://www.guokr.com/post/583663/}
+    \* \link+[无调性理论简介（六）音集的特性和音阶]{http://www.guokr.com/post/587919/}
+    \* \link+[无调性理论简介（七）十二音体系]{http://www.guokr.com/post/596904/}
+}
