297c992522a1bb4f1d75bcc46650bd4738ab2dbe
James Ruan <ruanbeihong@gmail.com>
2014-07-31 00:13:05 +0800

    add the file project_fgsocks.ml.js

---
+++ project_fgsocks.ml.js
@@ -0,0 +1,20 @@
+\h3{fgsocks \subh{in which F is for FUCK}}
+
+Our internet is becoming dangerous every day. 
+\"The big brother is wathing you\" and more: they limit our freedom of information by filtering the internet by many means.
+
+They use key words filtering to cut our tongue.\newline
+They ues domain name pollution to blind our eye.\newline
+They use CONNECTION RESET to kick us back to the IGNORANCE.\newline
+They now use Machine Learning to pick up the Wall Tunneler and crash them like crashing a bug.
+
+They have their enemies, but they kill millions people's freedom for one enemy.
+
+It is not only the GFW, but also the technical guys behind that evil plan. They developed the weapon to make the internet a Winter of Nuclear, in the near future. We need our defence or all of us will not survive the Winter.
+
+And we have to survive the Winter, and we need our tunnels for safety, not only to breach through the Wall but also a refuge to those who are not to be blinded, to be muted or to be fooled.
+
+That is the initiative for this project. This project is just a pilot to experiment techniques and ideas. The final goal it to build a distributed trusted tunneling network that will be used whenever the freedom of information is at stake.
+
+\link+[project on github]{https://github.com/jamesruan/fgsocks}.
+
